105203e3b3048f9879b800b02a772e98d33c6952
Nguyen Xuan An <lanehacker7294@yahoo.com>
2019-03-04 00:38:25 +0700

    final

--- src/Java/QuanLiDaySoNguyen.java
+++ src/Java/QuanLiDaySoNguyen.java
@@ -5,7 +5,24 @@ public class QuanLiDaySoNguyen {
         array.Nhap();
         array.Xuat();
         System.out.println();
+
         array.Sua();
+        System.out.println("Mang sau khi sua: ");
         array.Xuat();
+        System.out.println();
+
+        array.Them();
+        System.out.println("Mang sau khi them: ");
+        array.Xuat();
+        System.out.println();
+
+        array.Sort();
+        System.out.println("Mang sau khi sap tang: ");
+        array.Xuat();
+        System.out.println();
+        
+        array.Average();
+        int i=array.PTGanAvg();
+        System.out.println("Vi tri phan tu gan bang trung binh nhat: "+ i);
     }
 }

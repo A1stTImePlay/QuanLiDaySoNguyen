6d1dc358cf7f307320669db2dc88aef1db6cf089
Nguyen Xuan An <lanehacker7294@yahoo.com>
2019-03-03 19:35:32 +0700

    test

--- src/Java/DaySo.java
+++ src/Java/DaySo.java
@@ -4,45 +4,46 @@ public class DaySo {
     private int SoLuong;
     private int[] Mang;
 
-    public void Nhap(){
+    public void Nhap() {
         Scanner input = new Scanner(System.in);
         System.out.println("Nhap so luong cua mang: ");
-        this.SoLuong=input.nextInt();
+        this.SoLuong = input.nextInt();
         Mang = new int[SoLuong];
         System.out.println("Nhap mang: ");
-        for (int i=0;i<this.SoLuong;i++) {
-            Mang[i]= input.nextInt();
+        for (int i = 0; i < this.SoLuong; i++) {
+            Mang[i] = input.nextInt();
         }
     }
 
-    public void Xuat(){
+    public void Xuat() {
         System.out.println("Mang da nhap: ");
-        for (int i=0;i<this.SoLuong;i++) {
+        for (int i = 0; i < this.SoLuong; i++) {
             System.out.print(Mang[i]);
         }
     }
 
-    public void Sua(){
+    public void Sua() {
         Scanner input = new Scanner(System.in);
         System.out.println("Nhap vi tri so can sua: ");
-        int ViTri=input.nextInt();
+        int ViTri = input.nextInt();
         System.out.println("Nhap gia tri moi: ");
-        this.Mang[ViTri]=input.nextInt();
+        this.Mang[ViTri] = input.nextInt();
     }
 
-    public void Xoa(){
+    public void Xoa() {
 
     }
 
-    public void Them(){
+    public void Them() {
         Scanner input = new Scanner(System.in);
         System.out.println("Nhap so luong so them moi: ");
-        int SoThem=input.nextInt();
-        this.SoLuong+=SoThem;
+        int SoThem = input.nextInt();
+        this.SoLuong += SoThem;
 
     }
 
-    public void Sort(){
+    public void Sort() {
         Arrays.sort(this.Mang);
     }
+}
 
